6f2f33abb30a99ac170759898b50e01964a360fa
Prakash kumar singh <Pk87096639@gmail.com>
2024-05-10 12:24:55 +0530

    config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,15 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://co-share-client.vercel.app",
     credentials: true,
   })
 );
