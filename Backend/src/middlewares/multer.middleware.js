6edb932f732d605f2140a5c8dd75a31ec82c93d4
Prakash kumar singh <Pk87096639@gmail.com>
2024-05-14 15:36:14 +0530

    check

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -2,10 +2,13 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(
+      null,
+      `${Date.now()}-${Math.round(Math.random() * 1e9)}${file.originalname}`
+    );
   },
 });
 
