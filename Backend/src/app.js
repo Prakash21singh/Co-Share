f2ae972fa6831380bbe5135e3fd8755da3788348
Prakash kumar singh <Pk87096639@gmail.com>
2024-05-14 15:48:07 +0530

    check

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,19 +7,19 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
-    origin: "https://co-share-client.vercel.app",
-    // origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+// app.use(
+//   cors({
+//     origin: "https://co-share-client.vercel.app",
+//     // origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 export { app };
