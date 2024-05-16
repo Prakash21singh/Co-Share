a164f5eec1f350e90443fa5e477056bc6a547806
Prakash kumar singh <Pk87096639@gmail.com>
2024-05-16 13:04:57 +0530

    uploads download functionality created

--- Frontend/src/Pages/GlobalUpload/GlobalUpload.jsx
+++ Frontend/src/Pages/GlobalUpload/GlobalUpload.jsx
@@ -21,8 +21,31 @@ const GlobalUpload = () => {
       });
   }, []);
 
-  function handleFileDownload(url) {
-    console.log(url);
+  async function handleFileDownload(upload, filename) {
+    let uploadType = upload.split(".").at(-1);
+    if (uploadType === "pdf" || uploadType === "docx") {
+      const link = document.createElement("a");
+      link.href = upload;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      return;
+    }
+
+    if (uploadType === "jpeg" || uploadType === "png" || uploadType === "jpg") {
+      const response = await fetch(upload);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = filename;
+      link.style.display = "none";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    }
   }
 
   console.log(data[0]);
@@ -35,12 +58,15 @@ const GlobalUpload = () => {
               //abnhpm
               <div key={index} className="upload">
                 <div className="left">
-                  <img src={data.createdBy.avatar} alt="" />
+                  <img src={data.createdBy?.avatar} alt="" />
                 </div>
                 <div className="middle">
                   <h3>#{data.title}</h3>
                   <p>Description: {data.description}</p>
-                  <button onClick={() => handleFileDownload(data.upload)}>
+                  <button
+                    onClick={() => {
+                      handleFileDownload(data.upload, data.filename);
+                    }}>
                     Download
                   </button>
                 </div>
