b8de44f08e9798ce1ccaedc9d1f2c211e2c40c47
Prakash kumar singh <Pk87096639@gmail.com>
2024-05-21 07:11:51 +0530

    Production

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -2,13 +2,12 @@ import multer from "multer";
 import path from "path";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, "public", "temp"));
+    cb(null, "/public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
-console.log(__dirname, "Public", "temp");
 
 export const upload = multer({
   storage,
