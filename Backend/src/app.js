65a974c35af984c72c1f9feb58095a6e2a345f8e
Prakash kumar singh <Pk87096639@gmail.com>
2024-06-02 13:28:17 +0530

    created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,18 +7,12 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
+    // origin: "http://localhost:5173",
     origin: "https://co-share-client.vercel.app",
-    // origin: "*",
     credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
+
 export { app };
--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -8,6 +8,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 // const uploadOnCloudinay = async (localfilepath) => {
